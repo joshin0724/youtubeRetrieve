cc239ebd878af56f7c500131cdf2ef2528ae2581
joshin0724 <joshin0724@gmail.com>
2025-10-22 14:09:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ with main_search:
     st.markdown(
         """
         <p style='text-align: left; font-weight: bold; font-size: 1rem;'>
-        최근 1년 영상 중 가장 인기 있는(조회수) 순서로 보여드려요! 📈
+        검색어와 연관성이 높고, 최근 1년 영상 중 가장 인기 있는(조회수) 순서로 보여드려요! 📈
         </p>
         """,
         unsafe_allow_html=True
